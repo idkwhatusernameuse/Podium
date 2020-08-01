754a21c2a3f20345a48de3e029b3c0178f02d866
idkwhatusernameuse <idkwhatusernameuse@gmail.com>
2020-08-01 12:27:50 -0600

    (Fix) Gson would save the "data" variable instead of its value

--- app/src/main/java/dev/idkwuu/allesandroid/util/BookmarksManager.kt
+++ app/src/main/java/dev/idkwuu/allesandroid/util/BookmarksManager.kt
@@ -6,6 +6,7 @@ import com.google.gson.reflect.TypeToken
 
 class BookmarksManager {
     companion object {
+        @Suppress("RemoveExplicitTypeArguments")
         val data: MutableLiveData<MutableList<String>> by lazy {
             val type = object : TypeToken<MutableList<String>>() {}.type
             MutableLiveData(Gson().fromJson<MutableList<String>>(SharedPreferences.bookmarks, type))
@@ -14,11 +15,11 @@ class BookmarksManager {
 
     fun save(slug: String) {
         data.value?.add(slug)
-        SharedPreferences.bookmarks = Gson().toJson(data)
+        SharedPreferences.bookmarks = Gson().toJson(data.value)
     }
 
     fun remove(slug: String) {
         data.value?.remove(slug)
-        SharedPreferences.bookmarks = Gson().toJson(data)
+        SharedPreferences.bookmarks = Gson().toJson(data.value)
     }
 }
